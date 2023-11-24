cfc955920262f444b98f7bd9bb86333662d2ebd8
Quartzes-KAIST <quartzes@kaist.ac.kr>
2023-11-24 12:06:14 +0000

    update download code

--- fid-objaverse/download_objaverse.py
+++ fid-objaverse/download_objaverse.py
@@ -2,6 +2,9 @@ import objaverse
 import random
 import os
 import trimesh
+import yaml
+
+DOWNLOAD_PATH = f'/root/.objaverse/hf-objaverse-v1/objs'
 
 def get_objaverse_subset():
     with open("./fid-objaverse/objaverse_list.txt") as f:
@@ -61,5 +64,64 @@ def download_objaverse_subset():
         except:
             print(f'[Error] when converting {obj_path}')
 
+def get_objaverse_subset():
+	subset_list = dict()
+
+	with open("./fid-objaverse/objaverse_list.txt") as f:
+		for l in f.readlines():
+			id = l.rstrip().split("_")[-1]
+			subset_list[id] = '_'.join(l.rstrip().split("_")[:-1])
+			
+	return subset_list
+
+def get_data_list(base_path, guide_file_name):
+	data_list = [] # guide_path, id, name, object_path\
+	guide_list = []
+	
+	dir_list = os.listdir(base_path)
+	objaverse_subset = get_objaverse_subset()
+	
+	for dir in dir_list:
+		id_list = os.listdir(base_path + "/" + dir)
+		
+		for id in id_list:
+			name = objaverse_subset[id]
+			guide_path = base_path + "/" + dir + "/" + id + "/" + guide_file_name
+			object_path = base_path + "/" + dir + "/" + id + "/object.obj"
+			data_list.append([guide_path, id, name, object_path])
+			guide_list.append(guide_path)
+			
+	return data_list, guide_list
+
+def generate_guide(base_path, exp_path, guide_file_name, guide_list_name, is_rand = False):
+    base_path = DOWNLOAD_PATH
+    list_path = './fid-objaverse/' + guide_list_name
+    data_list, guide_list = get_data_list(base_path, guide_file_name)
+
+    for data in data_list:
+        guide_path = data[0]
+        id = exp_path + "/" + data[1]
+        name = f"{data[2].replace('_', ' ')}, {{}} view"
+        object_path = data[3]
+        
+        if os.path.exists(guide_path):
+            os.remove(guide_path)
+        
+        yaml_data = {'log': {'exp_name': id}, 'guide': {'text': name, 'append_direction': True, 'shape_path': object_path}, 'optim': {'seed': 3}}
+
+        if is_rand:
+            yaml_data['guide']['use_random_viewpoint'] = True
+
+        with open(guide_path, 'x') as file:
+            yaml.dump(yaml_data, file)
+            
+    if os.path.exists(list_path):
+        os.remove(list_path)
+
+    with open(list_path, 'x') as file:
+        file.write('\n'.join(guide_list))
+
 if __name__ == "__main__":
     download_objaverse_subset()
+    generate_guide(DOWNLOAD_PATH, 'normal', 'guide.yaml', 'guide_list.txt')
+    generate_guide(DOWNLOAD_PATH, 'random', 'guide_rand.yaml', 'guide_rand_list.txt', is_rand=True)
